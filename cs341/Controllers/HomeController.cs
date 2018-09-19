b818114bf2374084ae39718b919758f791495adc
Christian Strauss <strauss.christi@uwlax.edu>
2018-09-18 22:55:18 -0500

    added .net project base. if not familular the .cshtml in the views folder are html files essentially. Ill handle all the backend c# :D

---
+++ cs341/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using cs341.Models;
+
+namespace cs341.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
