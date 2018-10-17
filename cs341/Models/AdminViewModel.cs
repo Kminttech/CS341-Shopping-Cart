d5d91528c2cef9ac8a56c45fc834d2946cf8c014
Christian Strauss <castrauss501@gmail.com>
2018-10-16 23:06:47 -0500

    adminviewmodel

--- cs341/Models/AdminViewModel.cs
+++ cs341/Models/AdminViewModel.cs
@@ -1,35 +1,9 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
 
 namespace cs341.Models
 {
-    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
-    public class Middleware
+    public class AdminViewModel
     {
-        private readonly RequestDelegate _next;
-
-        public Middleware(RequestDelegate next)
-        {
-            _next = next;
-        }
-
-        public Task Invoke(HttpContext httpContext)
-        {
-
-            return _next(httpContext);
-        }
-    }
-
-    // Extension method used to add the middleware to the HTTP request pipeline.
-    public static class MiddlewareExtensions
-    {
-        public static IApplicationBuilder UseMiddlewareClassTemplate(this IApplicationBuilder builder)
-        {
-            return builder.UseMiddleware<Middleware>();
-        }
+        public User User { get; set; }
     }
 }
